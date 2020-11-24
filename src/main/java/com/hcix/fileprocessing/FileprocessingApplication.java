67188dd1317c79fadeed662270e482b0b766ab8f
Marel Oliva <marel.oliva@ideati.net>
2020-11-24 11:34:40 -0600

    First commit

---
+++ src/main/java/com/hcix/fileprocessing/FileprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.hcix.fileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileprocessingApplication.class, args);
+	}
+
+}
